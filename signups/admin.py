e076a9930a8652993a7bd090fc7d6889f7420cc6
myusuf <muhuboyusuf@gmail.com>
2015-01-25 00:24:44 +0000

    added signup model

--- signups/admin.py
+++ signups/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import SignUp
+
+class SignupAdmin(admin.ModelAdmin):
+    class Meta:
+        model = SignUp
+ #to be seen int the django admin
+admin.site.register(SignUp,SignupAdmin)

6d5d85c037a3a63f62fee43118e9ad3aa4bdb3bd
myusuf <muhuboyusuf@gmail.com>
2015-01-30 20:09:51 +0000

    view and template changes

--- mvp_landing/settings.py
+++ mvp_landing/settings.py
@@ -81,3 +81,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+TEMPLATE_DIR =(
+        os.path.join(os.path.dirname(BASE_DIR), "static", "templates"),
+)
--- mvp_landing/urls.py
+++ mvp_landing/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mvp_landing.views.home', name='home'),
+    url(r'^$', 'signups.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),

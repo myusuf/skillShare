d0cffec5038184fd2f5bbc7797a98ef77024137e
myusuf <muhuboyusuf@gmail.com>
2015-02-01 00:47:59 +0000

    changes to views and templates

--- mvp_landing/settings.py
+++ mvp_landing/settings.py
@@ -81,6 +81,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-TEMPLATE_DIR =(
-        os.path.join(os.path.dirname(BASE_DIR), "static", "templates"),
+#TEMPLATE_DIR= ('/vagrant/projects/mvp_landing/static/templates')
+TEMPLATE_DIRS =(
+        os.path.join(BASE_DIR, "static", "templates"),
 )

44f2000e55533cc9d1c68da1dadc90898483c086
myusuf <muhuboyusuf@gmail.com>
2015-02-01 00:48:43 +0000

    changes to views and templates

--- signups/views.py
+++ signups/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, RequestContext
 
 # Create your views here.
 def home(request):
-    return #something
+    return render_to_response("signup.html",
+                              locals(),
+                              context_instance=RequestContext(request))
